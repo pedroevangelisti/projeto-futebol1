d0e62f15bfe6e21a9cb0ee0ede2662d0f673b15c
pedroevangelisti <pedro.evangelisti@gmail.com>
2025-06-27 19:10:44 -0300

    Update back.py

--- back.py
+++ back.py
@@ -12,13 +12,5 @@ def search():
     team_name= request.form ['team']
     return jsonify({"time": team_name})
     
-from flask import Flask, request, jsonify
 
-# Vercel expects a function called `handler`
-def handler(request):
-    if request.method == "POST":
-        team_name = request.form.get("team", "")
-        return jsonify({"time": team_name})
-    else:
-        return jsonify({"error": "Only POST method allowed."}), 405
 
