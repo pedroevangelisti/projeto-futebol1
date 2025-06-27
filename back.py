bf4880f4d738fcd2b4cc4a965e5dc3fa94cc7acb
pedroevangelisti <pedro.evangelisti@gmail.com>
2025-06-27 19:02:04 -0300

    Add files via upload

---
+++ back.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, jsonify 
+import requests 
+
+app = Flask(__name__)
+
+@app.route ('/') #/ leva pro home 
+def home():
+    return render_template('index.html')
+
+@app.route ('/search', methods = ['POST']) #dados enviados por forms ou algo do tipo 
+def search():
+    team_name= request.form ['team']
+    return jsonify({"time": team_name})
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
